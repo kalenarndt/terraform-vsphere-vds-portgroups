ec76f21c6e048843c2b3649bb5e9ca7e66e131ce
kalenarndt <kalenarndt@gmail.com>
2021-12-24 18:23:17 +0000

    fix provider references

--- examples/multi-network/providers.tf
+++ examples/multi-network/providers.tf
@@ -6,3 +6,11 @@ terraform {
     }
   }
 }
+
+
+provider "vsphere" {
+  user                 = "administrator@vsphere.local"
+  password             = "mypassword"
+  vsphere_server       = "vc.myvc.com"
+  allow_unverified_ssl = true
+}
